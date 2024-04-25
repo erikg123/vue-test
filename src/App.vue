eca63cf4c3f5d7ad302194192b5a460b8056b624
Erik <erik.granbom@gmail.com>
2024-04-25 16:39:34 +0200

    routes and links

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,23 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterLink, RouterView } from 'vue-router';
 </script>
 
 <template>
-  <header>
-    <div>
-      <nav>
-        <RouterLink to="/"></RouterLink>
-        <!-- <RouterLink to="/"></RouterLink> -->
-      </nav>
-    </div>
+  <header class="flex justify-center">
+    <nav class="my-8 space-x-4 text-2xl">
+      <RouterLink
+        to="/"
+        class="font-semibold text-gray-300"
+        :class="{ 'text-black underline underline-offset-8': $route.path === '/' }"
+        >Visa</RouterLink
+      >
+      <RouterLink
+        to="/edit"
+        class="font-semibold text-gray-300"
+        :class="{ 'text-black underline underline-offset-8': $route.path === '/edit' }"
+        >Editera</RouterLink
+      >
+    </nav>
   </header>
-
   <RouterView />
 </template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,24 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: []
-})
+  routes: [
+    {
+      path: '/',
+      name: 'View',
+      component: () => import('../views/ViewTable.vue')
+    },
+    {
+      path: '/edit',
+      name: 'Edit',
+      component: () => import('../views/EditTable.vue')
+    }
+    // {
+    //   path: '/:pathMatch(.*)*',
+    //   name: 'NotFound',
+    //   component: () => import('../views/NotFound.vue')
+    // }
+  ]
+});
 
-export default router
+export default router;
