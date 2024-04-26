85bc841585b17b7f2fd87d64812a4e9e395a2ec3
Erik <erik.granbom@gmail.com>
2024-04-26 09:34:17 +0200

    styling

--- src/components/AddData.vue
+++ src/components/AddData.vue
@@ -20,25 +20,46 @@ const handleSubmitClick = () => {
 </script>
 
 <template>
-  <form class="mb-4 flex w-screen flex-col p-4">
+  <form class="mb-4 flex w-screen flex-col p-4" v-if="users.data">
     <label for="username">Användarnamn:</label>
-    <input id="username" type="text" class="max-w-60 rounded border" v-model="newUser.username" />
+    <input
+      id="username"
+      type="text"
+      class="max-w-60 rounded border p-1"
+      v-model="newUser.username"
+    />
 
     <label for="email">Epost:</label>
-    <input id="email" type="email" class="max-w-60 rounded border" v-model="newUser.email" />
+    <input id="email" type="email" class="max-w-60 rounded border p-1" v-model="newUser.email" />
 
     <label for="firstName">Förnamn:</label>
-    <input id="firstName" type="text" class="max-w-60 rounded border" v-model="newUser.firstName" />
+    <input
+      id="firstName"
+      type="text"
+      class="max-w-60 rounded border p-1"
+      v-model="newUser.firstName"
+    />
 
     <label for="lastName">Efternamn:</label>
-    <input id="lastName" type="text" class="max-w-60 rounded border" v-model="newUser.lastName" />
+    <input
+      id="lastName"
+      type="text"
+      class="max-w-60 rounded border p-1"
+      v-model="newUser.lastName"
+    />
 
     <label for="birthdate">Födelsedatum:</label>
-    <input id="birthdate" type="date" class="max-w-60 rounded border" v-model="newUser.birthdate" />
+    <input
+      id="birthdate"
+      type="date"
+      class="max-w-60 rounded border p-1"
+      v-model="newUser.birthdate"
+    />
 
     <button
       class="mt-4 w-28 rounded border border-black px-4 py-2 font-bold"
       @click.prevent="handleSubmitClick"
+      :disabled="users.loading"
     >
       Lägg till
     </button>
